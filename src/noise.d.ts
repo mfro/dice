e1d780f79f4f063ce054cabf90956d4393bfd844
Max Froehlich <max@mfro.me>
2021-12-06 16:25:18 -0500

    Clean up

--- src/noise.d.ts
+++
@@ -1,4 +0,0 @@
-export function simplex2(x: number, y: number): number;
-export function simplex3(x: number, y: number, z: number): number;
-export function perlin2(x: number, y: number): number;
-export function perlin3(x: number, y: number, z: number): number;
